6970f4c36e2c44bcfab7184d7adefd96397dd883
salamanderius77 <priceymeteor@gmail.com>
2026-06-07 12:59:14 +0200

    Update NovaDebugAddon.java

--- nova-debug-v2/src/main/java/dev/nova/novadebug/NovaDebugAddon.java
+++ nova-debug-v2/src/main/java/dev/nova/novadebug/NovaDebugAddon.java
@@ -1,10 +1,12 @@
 package dev.nova.novadebug;
 import dev.nova.novadebug.modules.GoofyDebug;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 public class NovaDebugAddon extends MeteorAddon {
     public static final String NAME = "Nova Debug v2";
     public static final String AUTHOR = "Saint";
+    public static final Category CATEGORY = new Category("Nova Debug");
     @Override
     public void onInitialize() {
         Modules.get().add(new GoofyDebug());
