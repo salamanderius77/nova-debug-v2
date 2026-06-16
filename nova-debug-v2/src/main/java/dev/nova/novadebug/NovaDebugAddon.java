b6fb31245d54a9c1b78f38f92e3670fe5703074c
salamanderius77 <priceymeteor@gmail.com>
2026-06-16 11:00:23 +0200

    Update NovaDebugAddon.java

--- nova-debug-v2/src/main/java/dev/nova/novadebug/NovaDebugAddon.java
+++ nova-debug-v2/src/main/java/dev/nova/novadebug/NovaDebugAddon.java
@@ -2,6 +2,7 @@ package dev.nova.novadebug;
 
 import dev.nova.novadebug.modules.SpawnerBeam;
 import dev.nova.novadebug.modules.PlayerSignal;
+import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -13,6 +14,8 @@ public class NovaDebugAddon extends MeteorAddon {
 
     @Override
     public void onInitialize() {
+        MeteorClient.EVENT_BUS.subscribe(SaintToast.get());
+
         Modules.get().add(new SpawnerBeam());
         Modules.get().add(new PlayerSignal());
     }
